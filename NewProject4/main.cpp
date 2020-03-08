e3ce53b36b08a2b27a52bad753b40aa9b5138867
Jiamin Xu <jiaminxu1019@gmail.com>
2020-03-07 18:26:52 -0800

    Initial Commit

---
+++ NewProject4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NewProject4
+//
+//  Created by Jiamin Xu on 3/7/20.
+//  Copyright © 2020 Jiamin Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
